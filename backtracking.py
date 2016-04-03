6c4ec81460b41e27b4b021070884cc157c9422c8
Josh Lemer <joshlemer@gmail.com>
2016-04-03 13:00:55 -0700

    valid_moves

--- backtracking.py
+++ backtracking.py
@@ -7,7 +7,7 @@ import time
 grid = None
 
 
-def main():
+def __main__():
 	# accepts size >= 5
 	for i in range(5, 6):
 		board_size = i
@@ -39,24 +39,25 @@ def find_tour(size, x, y, move_index):
 		# print("Max number of moves reached = %d" % move_index)
 		return True
 
-	for i in range(possible_moves):
-		x_next = x + moves[i][0]
-		y_next = y + moves[i][1]
+	# [ move1, move2, ...]
+	valid_moves = [move for move in moves if is_valid_move(x + move[0], y + move[1], size)]
+
+	for valid_move in valid_moves:
+		x_next = x + valid_move[0]
+		y_next = y + valid_move[1]
 		# print(grid)
 		# print("move#: %d, (x,y)=(%d,%d)" % (move_index, x, y))
-		if valid_move(x_next, y_next, size):
-			next_move = move_index+1
-			grid[x_next][y_next] = move_index
+		next_move = move_index+1
+		grid[x_next][y_next] = move_index
 
-			if find_tour(size, x_next, y_next, next_move):
-				return True
-			else:  # Backtrack the previous moves
-				grid[x_next][y_next] = -1
+		if find_tour(size, x_next, y_next, next_move):
+			return True
+		else:  # Backtrack the previous moves
+			grid[x_next][y_next] = -1
 
 	return False
 
-
-def valid_move(x, y, size):
+def is_valid_move(x, y, size):
 	global grid
 	valid = False
 	if 0 <= x < size and 0 <= y < size and grid[x][y] == -1:  # if move not visited and inside boundary
@@ -72,4 +73,4 @@ def print_tour(tour_found, board, size):
 	else:
 		print("None found.")
 
-main()
+__main__()
