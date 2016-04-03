713020ac1683b12a5310d30716747d1adc45082e
Shivani Tyagi <tashivani@123gmail.com>
2016-04-03 14:36:25 -0500

    warnsdorff.py

--- warnsdorff.py
+++ warnsdorff.py
@@ -1,52 +1,78 @@
+__author__ = 'shivani'
+# This is the Backtracking Algorithm that finds the Knight's Tour (open) using recursion
+# Python version: 3.4
+# Execution: python backtracking.py
+import time
+
+grid = None
+
+
 def main():
-	solveKnightsTour(5, 0, 0)
+	# accepts size >= 5
+	for i in range(5, 6):
+		board_size = i
+		knights_tour(board_size)
 
 
-def isValidMove(grid, x, y):
-	maxL = len(grid) - 1
-	if x <= maxL or y <= maxL or grid[x][y] > -1:
-		return False
-	return True
+def knights_tour(size):
+	global grid
+	grid = [[-1 for i in range(size)] for i in range(size)]  # grid initialized to -1
+
+	# starting position (0,0)
+	x = y = 0
+	grid[x][y] = 0
+
+	start = time.time()
+	found = find_tour(size, x, y, 1)
+	end = time.time()
+	print_tour(found, grid, size)
+	print("(took %f seconds)" % (end - start))
 
 
-def getValidMoves(grid, x, y, validMoves):
-	return [(i, j) for i, j in validMoves if isValidMove(grid, x + i, y + j)]
+def find_tour(size, x, y, move_index):
+	global grid
+	possible_moves = 8
+	# x_moves = [2, 1, -1, -2, -2, -1,  1,  2]  # the 8 possible moves from any point
+	# y_moves = [1, 2, 2,   1, -1, -2, -2, -1]
 
+	moves = [(2,1), (1,2), (-1,2), (-2,1), (-2,-1), (-1, -2), (1, -2), (2, -1)]
 
-def movesSortedbyNumNextValidMoves(grid, x, y, legalMoves):
-	nextValidMoves = [(i, j) for i, j in getValidMoves(grid, x, y, legalMoves)]
-	# find the number of valid moves for each of the possible valid mode from x,y
-	withNumNextValidMoves = [(len(getValidMoves(grid, x + i, y + j, legalMoves)), i, j) for i, j in nextValidMoves]
-	# sort based on the number so that the one with smallest number of valid moves comes on the top
-	return [(t[1], t[2]) for t in sorted(withNumNextValidMoves)]
-
-
-def _solveKnightsTour(grid, x, y, num, legalMoves):
-	if num == pow(len(grid), 2):
+	# base case:
+	if move_index == size*size:
+		# print("Max number of moves reached = %d" % move_index)
 		return True
-	# for i,j in movesSortedbyNumNextValidMoves(grid,x,y,legalMoves):
-	# For testing the advantage of warnsdorff heuristics, comment the above line and uncomment the below line
-	for i, j in getValidMoves(grid, x, y, legalMoves):
-		xN, yN = x + i, y + j
-		if isValidMove(grid, xN, yN):
-			grid[xN][yN] = num
-			if _solveKnightsTour(grid, xN, yN, num + 1, legalMoves):
+
+	for i in range(possible_moves):
+		x_next = x + moves[i][0]
+		y_next = y + moves[i][1]
+		# print(grid)
+		# print("move#: %d, (x,y)=(%d,%d)" % (move_index, x, y))
+		if valid_move(x_next, y_next, size):
+			next_move = move_index+1
+			grid[x_next][y_next] = move_index
+
+			if find_tour(size, x_next, y_next, next_move):
 				return True
-			grid[xN][yN] = -2
+			else:  # Backtrack the previous moves
+				grid[x_next][y_next] = -1
+
 	return False
 
 
-def solveKnightsTour(gridSize, startX=0, startY=0):
-	legalMoves = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]
-	# Initializing the grid
-	grid = [x[:] for x in [[-1] * gridSize] * gridSize]
-	print(grid)
-	grid[startX][startY] = 0
-	if _solveKnightsTour(grid, startX, startY, 1, legalMoves):
-		for row in grid:
-			print '  '.join(str(e) for e in row)
-	else:
-		print 'Could not solve the problem..'
+def valid_move(x, y, size):
+	global grid
+	valid = False
+	if 0 <= x < size and 0 <= y < size and grid[x][y] == -1:  # if move not visited and inside boundary
+		valid = True
+	return valid
 
+
+def print_tour(tour_found, board, size):
+	print("\nKnight's Tour (%dx%d):" % (size, size))
+	if tour_found:
+		for x in board:
+			print('\t'.join(map(str, x)))
+	else:
+		print("None found.")
 
 main()
