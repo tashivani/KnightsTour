a4e0e64b7afae07f28149722342ae1b377c853b2
Shivani Tyagi <tashivani@123gmail.com>
2016-04-03 15:07:58 -0500

    2warnsdorff.py

--- warnsdorff.py
+++ warnsdorff.py
@@ -7,7 +7,7 @@ import time
 grid = None
 
 
-def main():
+def __main__():
 	# accepts size >= 5
 	for i in range(5, 6):
 		board_size = i
@@ -28,38 +28,54 @@ def knights_tour(size):
 	print_tour(found, grid, size)
 	print("(took %f seconds)" % (end - start))
 
+def get_move_with_least_possible_moves(x, y, size):
+	moves = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
+	possible_moves = 8
+	index = 0
+	for i in range(8):
+		x_n = x + moves[i][0]
+		y_n = y + moves[i][1]
+		count = 0
+		if is_valid_move(x_n, y_n, size):
+			for j in range(8):
+				x_nn = x + moves[j][0]
+				y_nn = y + moves[j][1]
+				if is_valid_move(x_nn, y_nn, size):
+					count += 1
+			if count < possible_moves:
+				possible_moves = count
+				index = i
+	return index
 
 def find_tour(size, x, y, move_index):
 	global grid
 	possible_moves = 8
-	# x_moves = [2, 1, -1, -2, -2, -1,  1,  2]  # the 8 possible moves from any point
-	# y_moves = [1, 2, 2,   1, -1, -2, -2, -1]
-
-	moves = [(2,1), (1,2), (-1,2), (-2,1), (-2,-1), (-1, -2), (1, -2), (2, -1)]
+	moves = [(2, 1), (1, 2), (-1, 2), (-2, 1), (-2, -1), (-1, -2), (1, -2), (2, -1)]
 
 	# base case:
 	if move_index == size*size:
 		# print("Max number of moves reached = %d" % move_index)
 		return True
 
-	for i in range(possible_moves):
-		x_next = x + moves[i][0]
-		y_next = y + moves[i][1]
+	# [ move1, move2, ...]
+	valid_moves = [move for move in moves if is_valid_move(x + move[0], y + move[1], size)]
+
+	for valid_move in valid_moves:
+		x_next = x + valid_move[0]
+		y_next = y + valid_move[1]
 		# print(grid)
 		# print("move#: %d, (x,y)=(%d,%d)" % (move_index, x, y))
-		if valid_move(x_next, y_next, size):
-			next_move = move_index+1
-			grid[x_next][y_next] = move_index
+		next_move = move_index+1
+		grid[x_next][y_next] = move_index
 
-			if find_tour(size, x_next, y_next, next_move):
-				return True
-			else:  # Backtrack the previous moves
-				grid[x_next][y_next] = -1
+		if find_tour(size, x_next, y_next, next_move):
+			return True
+		else:  # Backtrack the previous moves
+			grid[x_next][y_next] = -1
 
 	return False
 
-
-def valid_move(x, y, size):
+def is_valid_move(x, y, size):
 	global grid
 	valid = False
 	if 0 <= x < size and 0 <= y < size and grid[x][y] == -1:  # if move not visited and inside boundary
@@ -75,4 +91,4 @@ def print_tour(tour_found, board, size):
 	else:
 		print("None found.")
 
-main()
+__main__()
